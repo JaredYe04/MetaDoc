90d1480499c2b5ba6268ef524519ed0d0b50061d
Jared Ye <87924680+JaredYe04@users.noreply.github.com>
2025-12-09 21:10:03 +0800

    fix: MetaInfo使用el-scrollbar

--- meta-doc/src/renderer/src/components/MetaInfoPanel.vue
+++ meta-doc/src/renderer/src/components/MetaInfoPanel.vue
@@ -1,140 +1,144 @@
 <template>
   <div class="meta-panel" :style="panelStyle">
-    <div class="meta-panel__header">
-      <el-tooltip :content="$t('article.edit_meta_info')" placement="left">
-        <h1
-          class="meta-panel__title interactive-text"
-          @click="metaDialogVisible = true"
-          :style="{ color: themeState.currentTheme.textColor }"
+    <el-scrollbar class="meta-panel__scroll" wrap-class="meta-panel__scroll-wrap">
+      <div class="meta-panel__content">
+        <div class="meta-panel__header">
+          <el-tooltip :content="$t('article.edit_meta_info')" placement="left">
+            <h1
+              class="meta-panel__title interactive-text"
+              @click="metaDialogVisible = true"
+              :style="{ color: themeState.currentTheme.textColor }"
+            >
+              {{ $t('article.meta_info') }}
+            </h1>
+          </el-tooltip>
+        </div>
+
+        <div
+          class="meta-panel__item interactive-text"
+          role="button"
+          tabindex="0"
+          @click="titleAssistantVisible = true"
+          @keydown.enter.prevent="titleAssistantVisible = true"
+          @keydown.space.prevent="titleAssistantVisible = true"
         >
-          {{ $t('article.meta_info') }}
-        </h1>
-      </el-tooltip>
-    </div>
+          <span class="meta-panel__label" :style="{ color: themeState.currentTheme.textColor }">{{ $t('article.title') }}：</span>
+          <span class="meta-panel__value" :style="{ color: themeState.currentTheme.textColor }">{{ meta.title || $t('article.no_title') }}</span>
+        </div>
 
-    <div
-      class="meta-panel__item interactive-text"
-      role="button"
-      tabindex="0"
-      @click="titleAssistantVisible = true"
-      @keydown.enter.prevent="titleAssistantVisible = true"
-      @keydown.space.prevent="titleAssistantVisible = true"
-    >
-      <span class="meta-panel__label" :style="{ color: themeState.currentTheme.textColor }">{{ $t('article.title') }}：</span>
-      <span class="meta-panel__value" :style="{ color: themeState.currentTheme.textColor }">{{ meta.title || $t('article.no_title') }}</span>
-    </div>
+        <div
+          class="meta-panel__item interactive-text"
+          role="button"
+          tabindex="0"
+          @click="authorAssistantVisible = true"
+          @keydown.enter.prevent="authorAssistantVisible = true"
+          @keydown.space.prevent="authorAssistantVisible = true"
+        >
+          <span class="meta-panel__label" :style="{ color: themeState.currentTheme.textColor }">{{ $t('article.author') }}：</span>
+          <span class="meta-panel__value" :style="{ color: themeState.currentTheme.textColor }">{{ meta.author || $t('article.no_author') }}</span>
+        </div>
 
-    <div
-      class="meta-panel__item interactive-text"
-      role="button"
-      tabindex="0"
-      @click="authorAssistantVisible = true"
-      @keydown.enter.prevent="authorAssistantVisible = true"
-      @keydown.space.prevent="authorAssistantVisible = true"
-    >
-      <span class="meta-panel__label" :style="{ color: themeState.currentTheme.textColor }">{{ $t('article.author') }}：</span>
-      <span class="meta-panel__value" :style="{ color: themeState.currentTheme.textColor }">{{ meta.author || $t('article.no_author') }}</span>
-    </div>
+        <div
+          class="meta-panel__item interactive-text"
+          role="button"
+          tabindex="0"
+          @click="descriptionAssistantVisible = true"
+          @keydown.enter.prevent="descriptionAssistantVisible = true"
+          @keydown.space.prevent="descriptionAssistantVisible = true"
+        >
+          <span class="meta-panel__label" :style="{ color: themeState.currentTheme.textColor }">{{ $t('article.description') }}：</span>
+          <span class="meta-panel__value" :style="{ color: themeState.currentTheme.textColor }">{{ meta.description || $t('article.no_description') }}</span>
+        </div>
 
-    <div
-      class="meta-panel__item interactive-text"
-      role="button"
-      tabindex="0"
-      @click="descriptionAssistantVisible = true"
-      @keydown.enter.prevent="descriptionAssistantVisible = true"
-      @keydown.space.prevent="descriptionAssistantVisible = true"
-    >
-      <span class="meta-panel__label" :style="{ color: themeState.currentTheme.textColor }">{{ $t('article.description') }}：</span>
-      <span class="meta-panel__value" :style="{ color: themeState.currentTheme.textColor }">{{ meta.description || $t('article.no_description') }}</span>
-    </div>
+        <div class="meta-panel__item meta-panel__item--keywords">
+          <span
+            class="meta-panel__label meta-panel__label--keywords"
+            :style="{ color: themeState.currentTheme.textColor }"
+            role="button"
+            tabindex="0"
+            :aria-busy="keywordsGenerating"
+            @click="handleKeywordsGenerate"
+            @keydown.enter.prevent="handleKeywordsGenerate"
+            @keydown.space.prevent="handleKeywordsGenerate"
+          >
+            {{ $t('article.keywords') }}：
+            <el-icon v-if="keywordsGenerating" class="meta-keywords__icon" :size="14">
+              <Loading />
+            </el-icon>
+          </span>
+          <div class="meta-panel__value meta-panel__value--keywords">
+            <KeywordInput
+              :model-value="meta.keywords || []"
+              :placeholder="$t('article.keywords_placeholder')"
+              @update:modelValue="emitUpdate({ keywords: $event })"
+            />
+            <p v-if="!(meta.keywords && meta.keywords.length)" class="meta-keywords__empty" :style="{ color: themeState.currentTheme.textColor }">
+              {{ $t('article.no_keywords') }}
+            </p>
+          </div>
+        </div>
 
-    <div class="meta-panel__item meta-panel__item--keywords">
-      <span
-        class="meta-panel__label meta-panel__label--keywords"
-        :style="{ color: themeState.currentTheme.textColor }"
-        role="button"
-        tabindex="0"
-        :aria-busy="keywordsGenerating"
-        @click="handleKeywordsGenerate"
-        @keydown.enter.prevent="handleKeywordsGenerate"
-        @keydown.space.prevent="handleKeywordsGenerate"
-      >
-        {{ $t('article.keywords') }}：
-        <el-icon v-if="keywordsGenerating" class="meta-keywords__icon" :size="14">
-          <Loading />
-        </el-icon>
-      </span>
-      <div class="meta-panel__value meta-panel__value--keywords">
-        <KeywordInput
-          :model-value="meta.keywords || []"
-          :placeholder="$t('article.keywords_placeholder')"
-          @update:modelValue="emitUpdate({ keywords: $event })"
+        <MetaFieldAssistant
+          v-if="titleAssistantVisible"
+          :visible="titleAssistantVisible"
+          :prompt="generateTitlePrompt(outlineJson)"
+          :title="$t('article.generate_title')"
+          :default-value="meta.title || ''"
+          :default-input-size="1"
+          @accept="(value) => emitUpdate({ title: normalizeStringValue(value) })"
+          @update:visible="titleAssistantVisible = $event"
         />
-        <p v-if="!(meta.keywords && meta.keywords.length)" class="meta-keywords__empty" :style="{ color: themeState.currentTheme.textColor }">
-          {{ $t('article.no_keywords') }}
-        </p>
+
+        <MetaFieldAssistant
+          v-if="authorAssistantVisible"
+          :visible="authorAssistantVisible"
+          :prompt="wholeArticleContextPrompt(markdown)"
+          :title="$t('article.modify_author')"
+          :default-value="meta.author || ''"
+          :auto-generate="false"
+          :allow-generate="false"
+          :default-input-size="1"
+          @accept="(value) => emitUpdate({ author: normalizeStringValue(value) })"
+          @update:visible="authorAssistantVisible = $event"
+        />
+
+        <MetaFieldAssistant
+          v-if="descriptionAssistantVisible"
+          :visible="descriptionAssistantVisible"
+          :prompt="generateDescriptionPrompt(outlineJson)"
+          :title="$t('article.generate_description')"
+          :default-value="meta.description || ''"
+          :default-input-size="10"
+          @accept="(value) => emitUpdate({ description: normalizeStringValue(value) })"
+          @update:visible="descriptionAssistantVisible = $event"
+        />
+
+        <el-dialog v-model="metaDialogVisible" :title="$t('article.edit_meta_info')" width="30%">
+          <el-form>
+            <el-form-item :label="$t('article.title')">
+              <el-input v-model="formState.title" autocomplete="off" class="aero-input" />
+            </el-form-item>
+            <el-form-item :label="$t('article.author')">
+              <el-input v-model="formState.author" autocomplete="off" class="aero-input" />
+            </el-form-item>
+            <el-form-item :label="$t('article.description')">
+              <el-input
+                type="textarea"
+                v-model="formState.description"
+                autocomplete="off"
+                resize="none"
+                :autoSize="{ minRows: 3, maxRows: 5 }"
+                class="aero-input"
+              />
+            </el-form-item>
+          </el-form>
+          <template #footer>
+            <el-button @click="metaDialogVisible = false" :style="{ color: themeState.currentTheme.textColor }">{{ $t('common.cancel') }}</el-button>
+            <el-button type="primary" @click="commitForm" :style="{ color: themeState.currentTheme.textColor }">{{ $t('common.confirm') }}</el-button>
+          </template>
+        </el-dialog>
       </div>
-    </div>
-
-    <MetaFieldAssistant
-      v-if="titleAssistantVisible"
-      :visible="titleAssistantVisible"
-      :prompt="generateTitlePrompt(outlineJson)"
-      :title="$t('article.generate_title')"
-      :default-value="meta.title || ''"
-      :default-input-size="1"
-      @accept="(value) => emitUpdate({ title: normalizeStringValue(value) })"
-      @update:visible="titleAssistantVisible = $event"
-    />
-
-    <MetaFieldAssistant
-      v-if="authorAssistantVisible"
-      :visible="authorAssistantVisible"
-      :prompt="wholeArticleContextPrompt(markdown)"
-      :title="$t('article.modify_author')"
-      :default-value="meta.author || ''"
-      :auto-generate="false"
-      :allow-generate="false"
-      :default-input-size="1"
-      @accept="(value) => emitUpdate({ author: normalizeStringValue(value) })"
-      @update:visible="authorAssistantVisible = $event"
-    />
-
-    <MetaFieldAssistant
-      v-if="descriptionAssistantVisible"
-      :visible="descriptionAssistantVisible"
-      :prompt="generateDescriptionPrompt(outlineJson)"
-      :title="$t('article.generate_description')"
-      :default-value="meta.description || ''"
-      :default-input-size="10"
-      @accept="(value) => emitUpdate({ description: normalizeStringValue(value) })"
-      @update:visible="descriptionAssistantVisible = $event"
-    />
-
-    <el-dialog v-model="metaDialogVisible" :title="$t('article.edit_meta_info')" width="30%">
-      <el-form>
-        <el-form-item :label="$t('article.title')">
-          <el-input v-model="formState.title" autocomplete="off" class="aero-input" />
-        </el-form-item>
-        <el-form-item :label="$t('article.author')">
-          <el-input v-model="formState.author" autocomplete="off" class="aero-input" />
-        </el-form-item>
-        <el-form-item :label="$t('article.description')">
-          <el-input
-            type="textarea"
-            v-model="formState.description"
-            autocomplete="off"
-            resize="none"
-            :autoSize="{ minRows: 3, maxRows: 5 }"
-            class="aero-input"
-          />
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <el-button @click="metaDialogVisible = false" :style="{ color: themeState.currentTheme.textColor }">{{ $t('common.cancel') }}</el-button>
-        <el-button type="primary" @click="commitForm" :style="{ color: themeState.currentTheme.textColor }">{{ $t('common.confirm') }}</el-button>
-      </template>
-    </el-dialog>
+    </el-scrollbar>
   </div>
 </template>
 
@@ -278,14 +282,28 @@ const commitForm = () => {
 <style scoped>
 .meta-panel {
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+  height: 100%;
   padding: 14px;
   border-radius: 10px;
   color: var(--meta-text-color);
   box-sizing: border-box;
   margin: 0;
+  overflow: hidden;
+}
+
+.meta-panel__scroll {
+  width: 100%;
+  height: 100%;
+}
+
+.meta-panel__scroll :deep(.el-scrollbar__wrap) {
+  max-height: 100%;
+}
+
+.meta-panel__content {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 
 .meta-panel__header {
